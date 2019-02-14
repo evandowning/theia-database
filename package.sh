eb3989410ebf9ff46967598e0a3f1ae44b7324fc
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-02-14 14:09:58 +0000

    updated packing script

--- package.sh
+++ package.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-fpm -s dir -t deb -n analysis_tools --config-files handler_database.cfg clear.sql create.sql neo4j.cron neo4j-load-csv.sh neo4j.conf \
-                                           consume_database.py=/usr/bin/ \
+fpm -s dir -t deb -n analysis_tools --config-files handler_neo4j.cfg handler_anomaly.cfg clear.sql create.sql neo4j.cron neo4j-load-csv.sh neo4j.conf \
+                                           create_neo4j_csv.py=/usr/bin/ \
+                                           anomaly_detector.py=/usr/bin/ \
                                            theia_anomaly.py=/usr/bin/ \
                                            theia_neo4j.py=/usr/bin/ \
                                            sensitive.py=/usr/bin/ \
-                                           handler_database.cfg=/etc/theia/ \
-                                           clear.sql=/etc/theia/ \
+                                           handler_neo4j.cfg=/etc/theia/ \
+                                           handler_anomaly.cfg=/etc/theia/ \
                                            create.sql=/etc/theia/ \
+                                           clear.sql=/etc/theia/ \
                                            neo4j.cron=/etc/theia/ \
                                            neo4j-load-csv.sh=/etc/theia/ \
                                            neo4j.conf=/etc/neo4j/
