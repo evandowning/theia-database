1d22e4a08a96b583376cc2f43cc4d6b1d0f9bb82
Evan Downing <2077950+evandowning@users.noreply.github.com>
2019-04-03 18:31:00 +0000

    ignore predicate2

--- neo4j-load-csv.sh
+++ neo4j-load-csv.sh
@@ -100,13 +100,9 @@ USING PERIODIC COMMIT 500
 LOAD CSV FROM 'file:///backward-edge-$2.csv' as line
 MERGE (n1:NODE {uuid: line[3]})
 MERGE (n2:NODE {uuid: line[4]})
-MERGE (n3:NODE {uuid: line[5]})
-WITH line,n1,n2,n3
+WITH line,n1,n2
 WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n2.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
 CREATE (n1)<-[:NODE {uuid:line[1], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]-(n2)
-WITH line,n1,n3
-WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n3.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
-CREATE (n1)<-[:NODE {uuid:line[1], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]-(n3);
 \""
 query $archive "$root/backward-edge-*" "\${QUERY}" ${IMPORT_DIR}/backward-edge-$2.csv
 
@@ -116,13 +112,9 @@ USING PERIODIC COMMIT 500
 LOAD CSV FROM 'file:///forward-edge-$2.csv' as line
 MERGE (n1:NODE {uuid: line[3]})
 MERGE (n2:NODE {uuid: line[4]})
-MERGE (n3:NODE {uuid: line[5]})
-WITH line,n1,n2,n3
+WITH line,n1,n2
 WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n2.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
 CREATE (n1)-[:NODE {uuid:line[1], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]->(n2)
-WITH line,n1,n3
-WHERE n1.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0' AND n3.uuid <> '0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
-CREATE (n1)-[:NODE {uuid:line[1], nodeType:line[0], type:line[2], ts:line[6], size:line[7], name:line[8]}]->(n3);
 \""
 query $archive "$root/forward-edge-*" "\${QUERY}" ${IMPORT_DIR}/forward-edge-$2.csv
 
